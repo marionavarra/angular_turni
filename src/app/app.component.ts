8ff47e869e0a40bda7ec6b39d37cc82feb994f30
Mario Navarra <navarra.mario@gmail.com>
2020-12-17 21:39:28 +0100

    Rapportini completati in visualizzazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { analyzeAndValidateNgModules, CompileShallowModuleMetadata } from '@angular/compiler';
 
+const DAY_MS = 60 * 60 * 24 * 1000;
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,76 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title = 'Angular Example';
-  giorni: any = [];
+  days: any = [];
+  giorni_del_mese: any = [];
+  primo_giorno_mese = new Date;
+  progetti: any = [] ;
+  orari: any = [] ;
+  righe: any = [];
 
   constructor(private httpClient: HttpClient){}
   ngOnInit(){
+    this.httpClient.get("assets/progetti.json").subscribe(data1 =>{this.progetti = data1.progetti})
     this.httpClient.get("assets/test1.json").subscribe(data =>{
-      console.log(data);
-      this.giorni = data["giorni"];
-      console.log(this.giorni);
-    })
+    this.days = data["giorni"];
+    this.giorni_del_mese = this.getMonthDays(this.primo_giorno_mese);
+    this.progetti.forEach(p => {        
+      let riga: { progetto: { codice: any; descrizione: string[]; }; giorno: any; orario: any; }[] = [];
+      this.giorni_del_mese.forEach((giorno_del_mese: any) => {
+        let trovato = false;
+        this.days.forEach((giorno: { giorno: any; }) => {
+          giorno.giorno = Number(giorno.giorno);
+          if(giorno.giorno === giorno_del_mese){
+            giorno.progetti.forEach((progetto: {progetto: any;}) => {
+            trovato = true;
+              if(p["codice progetto"] === progetto["codice progetto"])
+                riga.push({giorno: giorno.giorno, orario: progetto.orario});
+            })
+          }
+        });
+        if (!trovato){
+          riga.push({giorno: giorno_del_mese, orario: null})
+        }
+    });
+      this.righe.push({progetto: {codice: p["codice progetto"], descrizione: p["descrizione progetto"]},orari: riga});
+    });
+    console.log(this.days);    
+    
+    
+    console.log(this.primo_giorno_mese.getDate())
+    console.log(this.getCalendarStartDay())
+    /*console.log(this.getCalendarDays(this.primo_giorno_mese))*/
+    console.log(this.giorni_del_mese)
+    console.log(this.progetti)
+    console.log(this.righe)
+
+  })
+
+}
+  getCalendarStartDay(date = new Date) {
+    const [year, month] = [date.getFullYear(), date.getMonth()];
+    const firstDayOfMonth = new Date(year, month, 1).getTime();
+    return this.range(1,7)
+      .map(num => new Date(firstDayOfMonth - DAY_MS * num))
+      .find(dt => dt.getDay() === 1)
+  }
+  getCalendarDays(date = new Date) {
+    const calendarStartTime =  this.getCalendarStartDay(date).getTime();
+
+    return this.range(0, 41)
+      .map(num => new Date(calendarStartTime + DAY_MS * num));
+  }
+  getMonthDays(date = new Date) {
+    const [year, month] = [date.getFullYear(), date.getMonth()];
+    const firstDayOfMonth = new Date(year, month, 1).getTime();
+    const lastDayOfMonth = new Date(year, month+1, 0).getTime();
+    const numberOfDayOfMonth = new Date(year, month+1, 0).getDate();
+    return this.range(0, numberOfDayOfMonth - 1)
+      .map(num => new Date(firstDayOfMonth + DAY_MS * num).getDate());
+  }
+
+  private range(start: number, end: number, length = end - start + 1) {
+    return Array.from({ length }, (_, i) => start + i)
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
