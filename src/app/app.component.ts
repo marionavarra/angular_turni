9caf6473e7177340b9500202aa16c25ba98e5d6b
Mario Navarra <navarra.mario@gmail.com>
2020-12-20 00:40:05 +0100

    Added interaction - BASE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,19 +19,19 @@ export class AppComponent implements OnInit {
   righe: any = [];
 
   constructor(private httpClient: HttpClient){}
-  ngOnInit(){
-    this.httpClient.get("assets/progetti.json").subscribe(data1 =>{this.progetti = data1.progetti})
-    this.httpClient.get("assets/test1.json").subscribe(data =>{
+  ngOnInit(){ 
+    this.httpClient.get("assets/progetti.json").subscribe((data1: any) =>{this.progetti = data1.progetti})
+    this.httpClient.get("assets/test1.json").subscribe((data: any) =>{
     this.days = data["giorni"];
     this.giorni_del_mese = this.getMonthDays(this.primo_giorno_mese);
-    this.progetti.forEach(p => {        
-      let riga: { progetto: { codice: any; descrizione: string[]; }; giorno: any; orario: any; }[] = [];
+    this.progetti.forEach((p: any) => {        
+      let riga: {  giorno: any; orario: any; }[] = [];
       this.giorni_del_mese.forEach((giorno_del_mese: any) => {
         let trovato = false;
-        this.days.forEach((giorno: { giorno: any; }) => {
+        this.days.forEach((giorno: { giorno: any, progetti: Array<Object>; }) => {
           giorno.giorno = Number(giorno.giorno);
           if(giorno.giorno === giorno_del_mese){
-            giorno.progetti.forEach((progetto: {progetto: any;}) => {
+            giorno.progetti.forEach((progetto: any) => {
             trovato = true;
               if(p["codice progetto"] === progetto["codice progetto"])
                 riga.push({giorno: giorno.giorno, orario: progetto.orario});
@@ -42,21 +42,44 @@ export class AppComponent implements OnInit {
           riga.push({giorno: giorno_del_mese, orario: null})
         }
     });
-      this.righe.push({progetto: {codice: p["codice progetto"], descrizione: p["descrizione progetto"]},orari: riga});
+      this.righe.push({progetto: {id: p["id"], codice: p["codice progetto"], descrizione: p["descrizione progetto"]},orari: riga});
     });
-    console.log(this.days);    
-    
-    
+    /*console.log(this.days);    
+     
+      
     console.log(this.primo_giorno_mese.getDate())
     console.log(this.getCalendarStartDay())
-    /*console.log(this.getCalendarDays(this.primo_giorno_mese))*/
+    console.log(this.getCalendarDays(this.primo_giorno_mese))
     console.log(this.giorni_del_mese)
-    console.log(this.progetti)
-    console.log(this.righe)
+    console.log(this.progetti)*/
+    console.log(this.righe);
 
-  })
+  });
+  
 
+  
 }
+  updateOrario(codice_progetto: String, orario: {giorno: Number, orario: String},  event: any){
+    /*console.log(this.days);
+    console.log(codice_progetto);
+    console.log(orario.orario);
+    console.log(orario.giorno); */
+    this.days.forEach((giorno: { giorno: any, progetti: Array<Object>; }) => {
+      if(giorno.giorno === orario.giorno){
+        giorno.progetti.forEach((progetto: any) => {
+          if(progetto["codice progetto"] === codice_progetto){
+            console.log(event.target.textContent);
+            progetto.orario = event.target.textContent;
+            console.log(progetto);
+            console.log("Ho cambiato l'orario");
+          }
+        });
+      }
+    });
+    console.log(this.days);
+
+
+  }
   getCalendarStartDay(date = new Date) {
     const [year, month] = [date.getFullYear(), date.getMonth()];
     const firstDayOfMonth = new Date(year, month, 1).getTime();
@@ -64,8 +87,9 @@ export class AppComponent implements OnInit {
       .map(num => new Date(firstDayOfMonth - DAY_MS * num))
       .find(dt => dt.getDay() === 1)
   }
-  getCalendarDays(date = new Date) {
-    const calendarStartTime =  this.getCalendarStartDay(date).getTime();
+  getCalendarDays(date = new Date) { 
+
+      const calendarStartTime =  this.getCalendarStartDay(date)!.getTime();
 
     return this.range(0, 41)
       .map(num => new Date(calendarStartTime + DAY_MS * num));
